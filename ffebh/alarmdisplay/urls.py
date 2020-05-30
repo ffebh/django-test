34fc547823c746dda9c612e0c9de5a38d08e0c51
08Max07 <pi@raspberrypi4.fritz.box>
2020-05-30 12:03:24 +0200

    added uhr.html

--- ffebh/alarmdisplay/urls.py
+++ ffebh/alarmdisplay/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, UhrView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('uhr', UhrView.as_view(), name='uhr'),
 ]
