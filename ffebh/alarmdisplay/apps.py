03984c882ed05a5ef69f61855cc15f27bfaa9586
08Max07 <pi@raspberrypi4.fritz.box>
2020-05-30 16:58:08 +0200

    clear master

--- ffebh/alarmdisplay/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AlarmdisplayConfig(AppConfig):
-    name = 'alarmdisplay'
--- ffebh/alarmdisplay/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class AlarmdisplayTests(TestCase):
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
