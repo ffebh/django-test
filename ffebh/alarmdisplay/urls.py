08b228698bbe76fdf33fd42398e92b9d7f7626c3
08Max07 <pi@raspberrypi4.fritz.box>
2020-05-30 20:21:17 +0200

    added variable to template home which is defined in view

--- ffebh/alarmdisplay/urls.py
+++ ffebh/alarmdisplay/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+    path('', HomePageView, name='home'),
     path('uhr', UhrView.as_view(), name='uhr'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ffebh/alarmdisplay/views.py
+++ ffebh/alarmdisplay/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.http import Http404
+from django.template import Context, loader
 
 # Create your views here.
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+def HomePageView(request):
+    t = loader.get_template('home.html')
+    return HttpResponse(t.render({"inhalt":"Hier könnte Ihre Werbung stehen"}))
 
 class UhrView(TemplateView):
     template_name = 'uhr.html'
+
+
