69cabec640ef407724820fe983b7c7ba96510497
08Max07 <pi@raspberrypi4.fritz.box>
2020-05-30 16:53:24 +0200

    uhr

--- ffebh/alarmdisplay/urls.py
+++ ffebh/alarmdisplay/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, UhrView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-]
+    path('uhr', UhrView.as_view(), name='uhr'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ffebh/alarmdisplay/views.py
+++ ffebh/alarmdisplay/views.py
@@ -6,3 +6,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class UhrView(TemplateView):
+    template_name = 'uhr.html'
