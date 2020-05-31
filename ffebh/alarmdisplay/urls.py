f23128e3b878cb690d691f8302e2275c97103eca
08Max07 <pi@raspberrypi4.fritz.box>
2020-05-31 10:34:49 +0200

    added einsatz_view

--- ffebh/alarmdisplay/urls.py
+++ ffebh/alarmdisplay/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, UhrView
+from .views import HomePageView, UhrView, EinsatzView
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', HomePageView, name='home'),
     path('uhr', UhrView.as_view(), name='uhr'),
+    path('einsatz', EinsatzView, name='einsatz'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ffebh/alarmdisplay/views.py
+++ ffebh/alarmdisplay/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from django.http import Http404
 from django.template import Context, loader
-
+from django.conf import settings
+import os
 # Create your views here.
 
 def HomePageView(request):
     t = loader.get_template('home.html')
-    return HttpResponse(t.render({"inhalt":"Hier könnte Ihre Werbung stehen"}))
+    #read txt file in static folder
+    file = open(os.path.join(settings.STATICFILES_DIRS[0], 'test.txt'),mode='r')
+    inhalt = file.read()
+    file.close()
+
+    return HttpResponse(t.render({"inhalt": inhalt}))
 
 class UhrView(TemplateView):
     template_name = 'uhr.html'
+
+def EinsatzView(request):
+    t = loader.get_template('einsatz.html')
+    return HttpResponse(t.render({'inhalt':'Hier können Infos rein'}))
 
 
