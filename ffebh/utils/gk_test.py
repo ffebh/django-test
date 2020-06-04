6cb6dec403cc6a4cd011414278d1ec01c6e2957f
08Max07 <pi@raspberrypi4.fritz.box>
2020-06-04 10:07:00 +0200

    skript zur umrechnung der GK-coords

---
+++ ffebh/utils/gk_test.py
@@ -0,0 +1,3 @@
+from gkconverter import convert_GK_to_lat_long
+
+print(convert_GK_to_lat_long(4403950, 5539478))
---
+++ ffebh/utils/test.py
@@ -0,0 +1,5 @@
+from gkconverter import convert_GK_to_lat_long
+
+coords = convert_GK_to_lat_long(4404567, 5538321)
+
+print(coords[0],'\n',coords[1])
