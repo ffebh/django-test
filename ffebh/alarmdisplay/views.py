0a7587dca75d92de58d8b8e424bc11b9e3bf4a5c
08Max07 <pi@raspberrypi4.fritz.box>
2020-06-04 15:10:00 +0200

    new einsatz template

--- ffebh/alarmdisplay/views.py
+++ ffebh/alarmdisplay/views.py
@@ -21,8 +21,40 @@ class UhrView(TemplateView):
 
 def EinsatzView(request):
     t = loader.get_template('einsatz.html')
+    
+    #import mysql.connector
+    #mydb = mysql.connector.connect(
+    #host="localhost",
+    # user="user",
+    # passwd="password"
+    # )
+    # mycursor = mydb.cursor()
+
+    # mycursor.execute("select * from alarmdisplay.einsaetze order by id desc limit 1")
+
+    # db = mycursor.fetchall()
+
+    # for row in db:
+    #     id = row[0]
+    #     strasse = row[1]
+    #     ort = row[2]
+    #     schlagwort = row[3]
+    #     alarmzeit = row[4]
+    #     einsatzmittel = row[5]
+    #     lon = row[6]
+    #     lat = row[7]
+
+    id = 1
+    strasse = 'Musterstraße 1'
+    ort = 'Musterort'
+    schlagwort = 'Musterschlagwort'
+    alarmzeit = 'Musterzeit'
+    einsatzmittel = 'Mustereinsatzmittel'
+    lon = 'Musterlängengrad'
+    lat = 'Musterbreitengrad'
     #Demonstration zum Aufruf externer Python Skripte. Diese müssen vorher importiert werden siehe oben
-    i = now.now()
-    return HttpResponse(t.render({'inhalt':i}))
+    #i = ocr.scan()
+    data = {'id':id, 'strasse':strasse, 'ort':ort, 'schlagwort':schlagwort, 'alarmzeit':alarmzeit, 'einsatzmittel':einsatzmittel, 'lon':lon, 'lat':lat}
+    return HttpResponse(t.render(data))
 
 
---
+++ ffebh/database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE monitor;
+USE monitor;
+CREATE TABLE `einsatze` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`strasse` VARCHAR(40),
+	`ort` VARCHAR(20),
+	`schlagwort` VARCHAR(80),
+	`alarmzeit` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+	`einsatzmittel` VARCHAR(5000),
+	`lon` VARCHAR(20),
+	`lat` VARCHAR(20),
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+CREATE USER 'alarmdisplay'@'localhost' identified BY 'passwort';
+GRANT ALL PRIVILEGES ON monitor.einsaetze TO 'alarmdisplay'@'localhost';
+FLUSH PRIVILEGES;
